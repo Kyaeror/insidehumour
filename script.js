0f4624d7886398372ea2745c1e0eb55a5ee510cd
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-12-14 16:30:20 -0500

    josh sux

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function middleFinger(event){
+    // event.preventDefault()
+    const jonathanIsANerd = document.getElementById("jonathanIsANerd")
+    jonathanIsANerd.innerHTML = `
+    <img src="./assets/istockphoto-140472118-612x612.jpg">
+    `
+}
